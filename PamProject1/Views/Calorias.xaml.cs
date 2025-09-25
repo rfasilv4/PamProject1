45c312a36335f521ceb9d5ff017808f7c67e247a
rfasilv4 <rafaslima2019@gmail.com>
2025-09-25 17:26:12 -0300

    nao funfa

--- PamProject1/Views/Calorias.xaml.cs
+++ PamProject1/Views/Calorias.xaml.cs
@@ -1,9 +1,28 @@
+using Microcharts;
+using SkiaSharp;
+using Entry = Microcharts.ChartEntry;
+
 namespace PamProject1.Views;
 
 public partial class Calorias : ContentPage
 {
-	public Calorias()
-	{
-		InitializeComponent();
-	}
+    public Calorias()
+    {
+        InitializeComponent();
+
+        // Dados simulados: horas de treino por dia (domingo a sábado)
+        var entries = new[]
+        {
+            new Entry(1) { Label = "Dom", ValueLabel = "1h", Color = SKColors.Blue },
+            new Entry(0.5f) { Label = "Seg", ValueLabel = "0.5h", Color = SKColors.Green },
+            new Entry(1.5f) { Label = "Ter", ValueLabel = "1.5h", Color = SKColors.Red },
+            new Entry(2) { Label = "Qua", ValueLabel = "2h", Color = SKColors.Orange },
+            new Entry(1) { Label = "Qui", ValueLabel = "1h", Color = SKColors.Purple },
+            new Entry(0.75f) { Label = "Sex", ValueLabel = "0.75h", Color = SKColors.Cyan },
+            new Entry(1.25f) { Label = "Sab", ValueLabel = "1.25h", Color = SKColors.Magenta },
+        };
+
+        // Criação do gráfico de barras
+        barChart.Chart = new BarChart { Entries = entries };
+    }
 }
